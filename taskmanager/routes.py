acf78b9332094c8a46097ae7b416c051e4f0b240
YesCoops <coopandmgc@gmail.com>
2022-03-29 08:45:19 +0000

    Add env.py to get over db connectivity issue

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -2,6 +2,7 @@ from flask import render_template, request, redirect, url_for
 from taskmanager import app, db
 from taskmanager.models import Category, Task
 
+
 @app.route("/")
 def home():
     return render_template("tasks.html")
