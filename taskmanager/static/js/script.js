9c348da267ac68e86218751bc50d2a78b2d32878
YesCoops <coopandmgc@gmail.com>
2022-03-29 14:44:19 +0000

    created add task page and connected to DB

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,5 +1,17 @@
 
   document.addEventListener('DOMContentLoaded', function() {
+    // sidenav init
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
-  }); 
+
+    // datpicker init
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+        format: "dd mmmm, yyyy",
+        i18n: {done: "Select"}
+    });
+
+    //select init
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
+  });
