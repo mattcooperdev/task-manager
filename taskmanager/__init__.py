40b8441fd4ecfff2dce4c3f206472501ee397926
YesCoops <coopandmgc@gmail.com>
2022-03-30 13:29:17 +0000

    Updated __init__.py

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -9,17 +9,14 @@ if os.path.exists("env.py"):
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 
-if os.environ.get("DEVELOPMENT") == True:
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
-else: 
+if os.environ.get("DEVELOPMENT") == "True":
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")  # local
+else:
     uri = os.environ.get("DATABASE_URL")
-    if uri.startswith("postgres//"):
+    if uri.startswith("postgres://"):
         uri = uri.replace("postgres://", "postgresql://", 1)
-    app.config["SQLALCHEMY_DATABASE_URI"] = uri #heroku
+    app.config["SQLALCHEMY_DATABASE_URI"] = uri  # heroku
 
 db = SQLAlchemy(app)
 
 from taskmanager import routes  # noqa
-
-
-
